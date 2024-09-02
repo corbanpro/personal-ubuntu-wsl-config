30a75ec4bf076d5461c3fb695e5f7848851224ca
corban <corbanpro@gmail.com>
2024-09-02 12:27:28 -0600

    add finder

--- .init.sh
+++ .init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cp -r ~/personal-ubuntu-config/.bashrc ~/.bashrc
 cp -r ~/personal-ubuntu-config/.dir_colors ~/dir_colors
-bash ~/.bashrc
+source ~/.bashrc
 sudo update
 sudo upgrade
-sudo apt install unzip ripgrep luarocks nodejs gh #build-essential python3-neovim tree-sitter-cli
+sudo apt install unzip ripgrep luarocks nodejs gh fd-find #build-essential python3-neovim tree-sitter-cli
 npm install -g typescript typescript-language-server emmet-ls
 bash ~/bash/addnvim.sh
 gh auth login

7cdc838069984f4716d3d5d01a225efa975881da
corban <corbanpro@gmail.com>
2024-09-03 15:33:36 -0600

    fix finder config

--- .init.sh
+++ .init.sh
@@ -5,8 +5,7 @@ curl -fsSL https://fnm.vercel.app/install | bash
 source ~/.bashrc
 fnm use --install-if-missing 20
 sudo apt install unzip ripgrep luarocks gh fd-find #build-essential python3-neovim tree-sitter-cli
-mkdir -p ~/.local/bin/fd
-ln -s $(which fdfind) ~/.local/bin/fd
+sudo mv /bin/fdfind /bin/fd
 npm install -g typescript typescript-language-server emmet-ls
 bash ~/bash/addnvim.sh
 gh auth login

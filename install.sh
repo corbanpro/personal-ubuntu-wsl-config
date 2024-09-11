6eee947d4837b6fb8b118a56daa8e57d3522a312
corban <corbanpro@gmail.com>
2024-09-10 20:46:01 -0600

    break init.sh into move_files.sh and install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-sudo update
-sudo upgrade
+sudo apt update
+sudo apt upgrade
 
 curl -fsSL https://fnm.vercel.app/install | bash
 source ~/.bashrc
-fnm use --install-if-missing 20
 
 sudo apt install unzip ripgrep luarocks gh fd-find openssl libssl-dev #build-essential python3-neovim tree-sitter-cli
 sudo mv /bin/fdfind /bin/fd
-npm install -g typescript typescript-language-server emmet-ls
+npm install -g typescript typescript-language-server emmet-ls neovim
 bash ~/bash/addnvim.sh
 
-cargo cargo-shuttle install
+fnm use --install-if-missing 20
+cargo install cargo-shuttle
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
-rm -rf lazygit lazygit.tar.gz 
+rm -rf lazygit lazygit.tar.gz
 
 gh auth login
---
+++ move_files.sh
@@ -0,0 +1,6 @@
+cp ~/.config/ubuntu/.bashrc ~/ 
+cp ~/.config/ubuntu/.dir_colors ~/
+cp ~/.config/ubuntu/.hushlogin ~/
+cp ~/.config/ubuntu/.vimrc ~/
+cp ~/.config/ubuntu/.inputrc ~/
+cp -r ~/.config/ubuntu/bash/ ~/ 

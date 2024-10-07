f9e1cedf268512798856b9ccea23e90c2728a14d
corban <corbanpro@gmail.com>
2024-10-06 20:26:22 -0600

    no changes

--- update_config.sh
+++ update_config.sh
@@ -1,6 +1,6 @@
- cp ~/.bashrc ~/.config/ubuntu
- cp ~/.dir_colors ~/.config/ubuntu
- cp ~/.hushlogin ~/.config/ubuntu
- cp ~/.vimrc ~/.config/ubuntu
- cp ~/.inputrc ~/.config/ubuntu
- cp -r ~/bash/ ~/.config/ubuntu
+cp ~/.bashrc ~/.config/ubuntu
+cp ~/.dir_colors ~/.config/ubuntu
+cp ~/.hushlogin ~/.config/ubuntu
+cp ~/.vimrc ~/.config/ubuntu
+cp ~/.inputrc ~/.config/ubuntu
+cp -r ~/bash/ ~/.config/ubuntu

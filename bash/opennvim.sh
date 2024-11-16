ae602eb55110627eb49ca96d3c293d86328d8f15
corban <corbanpro@gmail.com>
2024-11-15 22:38:38 -0700

    update open nvim script

--- bash/opennvim.sh
+++ bash/opennvim.sh
@@ -1,9 +1,10 @@
 if [[ $# -eq 0 ]]; then
-	if [[ -f "./.session.vim" ]]; then
-		'/usr/local/nvim-linux64/bin/nvim' -S ./.session.vim
-	else
-		'/usr/local/nvim-linux64/bin/nvim'
-	fi
+    SESSION_PATH=~/.local/share/nvim/sessions$PWD/Session.vim
+    if [ -f $SESSION_PATH ]; then
+        '/usr/local/nvim-linux64/bin/nvim' -S $SESSION_PATH
+    else
+        '/usr/local/nvim-linux64/bin/nvim'
+    fi
 else
-	'/usr/local/nvim-linux64/bin/nvim' $*
+    '/usr/local/nvim-linux64/bin/nvim' $*
 fi

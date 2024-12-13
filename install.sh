e7e0746205e9ceff7330477e4bd8d86fd0d6e238
corban <corbanpro@gmail.com>
2024-12-12 19:18:17 -0700

    update packages to install

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ sudo apt upgrade
 curl -fsSL https://fnm.vercel.app/install | bash
 source ~/.bashrc
 
-sudo apt install unzip ripgrep luarocks gh fd-find openssl libssl-dev python3.12-venv jq tmux net-tools #build-essential python3-neovim tree-sitter-cli
+sudo apt install unzip ripgrep luarocks gh fd-find openssl libssl-dev python3.12-venv jq tmux net-tools jq #build-essential python3-neovim tree-sitter-cli
 sudo mv /bin/fdfind /bin/fd
-npm install -g typescript typescript-language-server emmet-ls neovim jsdoc live-server
+npm install -g typescript typescript-language-server emmet-ls neovim jsdoc live-server tsx jest aws-cdk nodemon
 bash ~/bash/addnvim.sh
 
 fnm use --install-if-missing 20
-# cargo install cargo-shuttle
+# cargo install cargo-shuttle cargo-script cmd-wrapped
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -27,5 +27,3 @@ cd ~
 rm -rf temp/autojump
 
 gh auth login
-
-mkdir documents documents/side-projects documents/side-projects documents/work documents/work/byu-dev

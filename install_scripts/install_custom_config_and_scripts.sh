1648a6001340a109cdaedba7e5b80e7c9487f3a1
corban <corbanpro@gmail.com>
2025-04-11 12:37:31 -0600

    update custom install script

--- install_scripts/install_custom_config_and_scripts.sh
+++ install_scripts/install_custom_config_and_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp ../home_dir/{.**,**} ~/
-
-mkdir ~/.scripts
-git clone https://github.com/corbanpro/misc-scripts.git ~/.scripts
-chmod a+x ~/.scripts/*.sh
+cp -r ./home_dir/.** ~/ &&
+	rm -rf ~/.scripts/ &&
+	mkdir -p ~/.scripts &&
+	git clone https://github.com/corbanpro/misc-scripts.git ~/.scripts &&
+	chmod a+x ~/.scripts/*.sh

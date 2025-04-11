66079d0a7227e131b56ab14769b11105bf21ad84
corban <corbanpro@gmail.com>
2025-04-11 12:33:05 -0600

    update script

--- reset.sh
+++ reset.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./install_scripts/install_custom_config_and_scripts.sh
-./install_scripts/install_apt_packages.sh
-./install_scripts/install_node_npm.sh
-./install_scripts/install_rust.sh
-./install_scripts/install_lazygit.sh
-./install_scripts/install_autojump.sh
-./install_scripts/install_csharp.sh
-./install_scripts/github_login.sh
-~/.scripts/addnvim.sh
+./install_scripts/install_custom_config_and_scripts.sh &&
+	./install_scripts/install_apt_packages.sh &&
+	./install_scripts/install_node_npm.sh &&
+	./install_scripts/install_rust.sh &&
+	./install_scripts/install_lazygit.sh &&
+	./install_scripts/install_autojump.sh &&
+	./install_scripts/install_csharp.sh &&
+	./install_scripts/github_login.sh &&
+	~/.scripts/addnvim.sh
